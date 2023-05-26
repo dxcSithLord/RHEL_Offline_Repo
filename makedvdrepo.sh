5e5d6b17d4a54de43e69799b149a43b71226d55b
Alistair Amabile <48163392+dxcSithLord@users.noreply.github.com>
2023-05-26 16:51:29 +0100

    Changed for RHEL8 structure
    InstallMedia is incorrect, as there are two repo directories on the source DVD - BaseOS and AppStream.
    This update accomodates this change.

--- makedvdrepo.sh
+++ makedvdrepo.sh
@@ -3,8 +3,9 @@
 # Author: A.J.Amabile
 # Date: 9th April 2021
 # Original idea from https://access.redhat.com/soltions/1355683
+# Updated for RHEL8 25th May 2023
 
-OSver="rhel7"
+OSver="rhel8"
 DVDMount="/media/${OSver}"
 repoName="${OSver}dvd"
 repofile="${repoName}.repo"
@@ -16,7 +17,9 @@ $(mount | grep -E "^/dev/sr0 on ${DVDMount}" -q) || \
    sudo mount -o ro /dev/sr0 "${DVDMount}" || exit "Cant mount DVD"
 
 [[ -f /etc/yum.repos.d/${repofile} ]] && exit 0
-sed -e 's/gpgcheck=0/gpgcheck=1/' "${DVDMount}/media.repo" > ~/${repofile}
+# added change to InstallMedia name
+sed -e 's/gpgcheck=0/gpgcheck=1/' \
+    -e 's/InstallMedia/InstallMedia_BaseOS/' "${DVDMount}/media.repo" > ~/${repofile}
 #
 # function to add line to file is not already in the file
 #
@@ -30,6 +33,12 @@ testnset () {
 testnset "enabled" "1"
 testnset "baseurl" "file://${DVDMount}"
 testnset "gpgkey" "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release"
+
+# Add blank line and duplicate the file, with a change from BaseOS to AppStream
+# This allows both BaseOS and AppStream repositories on base DVD to be available
+# when mounted
+echo "" >> ~/${repofile}
+sed -e 's/BaseOS/AppStream/' ~/${repofile} >> ~/${repofile}
 
 sudo cp ~/${repofile} /etc/yum.repos.d/.
 sudo chmod 644 /etc/yum.repos.d/${repofile}
