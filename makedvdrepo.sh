715ca918b963ac858461959676e462cb55d2e643
Alistair Amabile <48163392+dxcSithLord@users.noreply.github.com>
2023-06-27 15:13:46 +0100

    Update makedvdrepo.sh

--- makedvdrepo.sh
+++ makedvdrepo.sh
@@ -3,27 +3,45 @@
 # Author: A.J.Amabile
 # Date: 9th April 2021
 # Original idea from https://access.redhat.com/soltions/1355683
+# Updated for RHEL8 25th May 2023
+# Updated with comments and frendly user info
+# Updated commands to include full path
 
-OSver="rhel7"
+OSver="rhel8"
 DVDMount="/media/${OSver}"
 repoName="${OSver}dvd"
 repofile="${repoName}.repo"
 #
 # test if already run
 #
-[[ -d "${DVDMount}" ]]  ||  sudo mkdir -p "${DVDMount}" 
-$(mount | grep -E "^/dev/sr0 on ${DVDMount}" -q) || \
-   sudo mount -o ro /dev/sr0 "${DVDMount}" || exit "Cant mount DVD"
+/usr/bin/echo "Checking for media mount point at ${DVDMount}"
+[[ -d "${DVDMount}" ]]  ||  /usr/bin/sudo /usr/bin/mkdir -p "${DVDMount}" 
+/usr/bin/echo "Mounting media (sudo required)"
+$(/usr/bin/mount | /usr/bin/grep -E "^/dev/sr0 on ${DVDMount}" -q) || \
+   /usr/bin/sudo /usr/bin/mount -o ro /dev/sr0 "${DVDMount}" || exit "Cant mount DVD"
 
-[[ -f /etc/yum.repos.d/${repofile} ]] && exit 0
-sed -e 's/gpgcheck=0/gpgcheck=1/' "${DVDMount}/media.repo" > ~/${repofile}
+/bin/echo "Media mounted on ${DVDMount}"
+
+[[ -f /etc/yum.repos.d/${repofile} ]] \
+  && /usr/bin/echo "Repositories file already in place, exiting" \
+  && exit 0
+#
+# DVD mounted and repofile does not yet exist, so create one from the DVD media.repo file
+# which is formatted for RHEL7 on RHEL8 DVD, without reference to new BaseOS and AppStream 
+# repositories.
+#
+/usr/bin/echo "Creating new DVD repository file ${repofile}, with repositories:"
+/usr/bin/echo "InstallMedia_BaseOS and InstallMedia_AppStream"
+# added change to InstallMedia name for RHEL8
+/usr/bin/sed -e 's/gpgcheck=0/gpgcheck=1/' \
+    -e 's/InstallMedia/InstallMedia_BaseOS/' "${DVDMount}/media.repo" > ~/${repofile}
 #
 # function to add line to file is not already in the file
 #
 testnset () {
   key=$1
   val=$2
-  grep "${key}=${val}" ~/${repofile} || echo "${key}=${val}" >> ~/${repofile}
+  /usr/bin/grep "${key}=${val}" ~/${repofile} || /usr/bin/echo "${key}=${val}" >> ~/${repofile}
   return $?
 }
 
@@ -31,8 +49,17 @@ testnset "enabled" "1"
 testnset "baseurl" "file://${DVDMount}"
 testnset "gpgkey" "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release"
 
-sudo cp ~/${repofile} /etc/yum.repos.d/.
-sudo chmod 644 /etc/yum.repos.d/${repofile}
+# Add blank line and duplicate the file, with a change from BaseOS to AppStream
+# This allows both BaseOS and AppStream repositories on base DVD to be available
+# when mounted
+/usr/bin/echo "" >> ~/${repofile}
+/usr/bin/sed -e 's/BaseOS/AppStream/' ~/${repofile} >> ~/${repofile}
 
-sudo yum clean all
-sudo yum repolist enabled
+/usr/bin/sudo /usr/bin/cp ~/${repofile} /etc/yum.repos.d/.
+/usr/bin/sudo /usr/bin/chmod 644 /etc/yum.repos.d/${repofile}
+
+/usr/bin/sudo /usr/bin/yum clean all
+/usr/bin/sudo /usr/bin/yum repolist enabled
+/usr/bin/echo "To use the DVD repositories, use :"
+/usr/bin/echo "dnf --enablerepo=InstallMedia_* ..."
+/usr/bin/echo "Re-run this command to re-mount media"
